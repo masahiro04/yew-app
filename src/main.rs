d82d6f255a8bc811f7cedbe8344d8d4d3856ebac
masahirookubo <stmhamachiii@gmail.com>
2022-08-17 00:46:55 +0900

    added state management

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,53 @@
 use yew::prelude::*;
 
+#[derive(Clone, PartialEq)]
 struct Video {
     id: usize,
     title: String,
     speaker: String,
     url: String,
+}
+
+#[derive(Clone, Properties, PartialEq)]
+struct VideosListProps {
+    videos: Vec<Video>,
+    on_click: Callback<Video>,
+}
+
+#[derive(Clone, Properties, PartialEq)]
+struct VideosDetailsProps {
+    video: Video,
+}
+
+#[function_component(VideosList)]
+fn vides_list(VideosListProps { videos, on_click }: &VideosListProps) -> Html {
+    let on_click = on_click.clone();
+    return videos
+        .iter()
+        .map(|video| {
+            let on_video_select = {
+                let on_click = on_click.clone();
+                let video = video.clone();
+                Callback::from(move |_| {
+                    on_click.emit(video.clone());
+                })
+            };
+
+            html! {
+                <p onclick={on_video_select}>{format!("{}: {}",video.speaker, video.title)}</p>
+            }
+        })
+        .collect::<Html>();
+}
+
+#[function_component(VideoDetails)]
+fn video_details(VideosDetailsProps { video }: &VideosDetailsProps) -> Html {
+    html! {
+        <div>
+            <h3>{video.title.clone()}</h3>
+            <img src="https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder" alt="video thumbnail"  />
+        </div>
+    }
 }
 
 #[function_component(App)]
@@ -36,26 +79,27 @@ fn app() -> Html {
         },
     ];
 
-    let videos = videos
-        .iter()
-        .map(|video| {
-            html! {
-                <p>{format!("{}: {}",video.speaker, video.title)}</p>
-            }
-        })
-        .collect::<Html>();
+    let selected_video = use_state(|| None);
+
+    let on_video_select = {
+        let selected_video = selected_video.clone();
+        Callback::from(move |video: Video| selected_video.set(Some(video)))
+    };
+
+    let details = selected_video.as_ref().map(|video| {
+        html! {
+            <VideoDetails video={video.clone()} />
+        }
+    });
 
     html! {
         <>
             <h1>{ "RustConf Explorer" }</h1>
             <div>
                 <h3>{"Videos to watch"}</h3>
-                <p>{ "John Doe: Building and breaking things" }</p>
-                <p>{ "Jane Smith: The development process" }</p>
-                <p>{ "Matt Miller: The Web 7.0" }</p>
-                <p>{ "Tom Jerry: Mouseless development" }</p>
-                { videos }
+                <VideosList videos={videos} on_click={on_video_select.clone()} />
             </div>
+            { for details }
             <div>
                 <h3>{ "John Doe: Building and breaking things" }</h3>
                 <img src="https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder" alt="video thumbnail" />
